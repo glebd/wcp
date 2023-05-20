5b8179ea0acbcbb00f6e3175cffa2fdb9c69cf5b
Gleb Dolgich <glebd70@gmail.com>
2023-05-21 00:01:31 +0100

    Keep source in project root dir

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release")
 
 target("wcp")
     set_kind("binary")
-    add_files("src/*.cpp")
+    add_files("*.c")
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
